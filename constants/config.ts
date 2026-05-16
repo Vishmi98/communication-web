41b972efab04f247c667bf25e83c6c426a7bddcd
Vishmi <vishmi@fedolab.com>
2026-05-16 17:18:44 +0530

    fixed url

--- constants/config.ts
+++ constants/config.ts
@@ -1,3 +1,3 @@
-export const URL = 'http://localhost:3000/api'
-// export const URL = 'https://communication-web-mu.vercel.app/api'
+// export const URL = 'http://localhost:3000/api'
+export const URL = 'https://communication-web-mu.vercel.app/api'
 
